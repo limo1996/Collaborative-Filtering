f344649c34bef764e25bcab797402a176b8e9de2
Jakub Lichman <lichman.jakub@gmail.com>
2018-06-23 16:43:27 +0200

    Both SVD implementations reworked

--- src/svd.py
+++ src/svd.py
@@ -5,69 +5,62 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-from six.moves import cPickle as pickle
-from performance import performance
+from utils import buildMatrix
+from AlgoBase import AlgoBase
 
-# load data from the file
-with open('../data/data.pickle', 'rb') as f:
-    data = pickle.load(f)
+class SVD(AlgoBase):
+    """
+    Naive SVD for collaborative filtering implementation.
 
-train_data = data['train_data']
-valid_data = data['valid_data']
-mean = data['mean']
+    k: top k dimensions to keep.
+    """
+    def __init__(self, k):
+        self.k = k
+        AlgoBase.__init__(self)
 
-# do SVD on them
-U, S, Vt = np.linalg.svd(train_data, full_matrices=True)
-eigvals = S**2 / np.cumsum(S)[-1]
+    def train(self):
+        self.split_data(0.8)
+        matrix = buildMatrix(self.train_data)
+        # do SVD on them
+        U, S, Vt = np.linalg.svd(matrix, full_matrices=True)
+        self.eigvals = S**2 / np.cumsum(S)[-1]
+        self.sing_vals = np.arange(matrix.shape[1]) + 1
+        self.predictions = self.reconstruct(U, S, Vt, self.k, matrix.shape)
 
-"""
-# Display scree plot
-fig = plt.figure(figsize=(8,5))
-sing_vals = np.arange(train_data.shape[1]) + 1
-plt.plot(sing_vals, eigvals, 'ro-', linewidth=2)
-plt.title('Scree Plot')
-plt.xlabel('Principal Component')
-plt.ylabel('Eigenvalue')
-# I don't like the default legend so I typically make mine like below, e.g.
-# with smaller fonts and a bit transparent so I do not cover up data, and make
-# it moveable by the viewer in case upper-right is a bad place for it 
-leg = plt.legend(['Eigenvalues from SVD'], loc='best', borderpad=0.3, 
+    def predict(self, user, item, verbose=False):
+        """ Predicts rating for (user, item) pair """
+        return self.predictions[user, item]
+
+    def plot_scree(self):
+        """ Plots scree plot """
+        # Display scree plot
+        fig = plt.figure(figsize=(8,5))
+        plt.plot(self.sing_vals, self.eigvals, 'ro-', linewidth=2)
+        plt.title('Scree Plot')
+        plt.xlabel('Principal Component')
+        plt.ylabel('Eigenvalue')
+        # I don't like the default legend so I typically make mine like below, e.g.
+        # with smaller fonts and a bit transparent so I do not cover up data, and make
+        # it moveable by the viewer in case upper-right is a bad place for it 
+        leg = plt.legend(['Eigenvalues from SVD'], loc='best', borderpad=0.3, 
                  shadow=False, prop=matplotlib.font_manager.FontProperties(size='small'),
                  markerscale=0.4)
-leg.get_frame().set_alpha(0.4)
-leg.draggable(state=True)
-plt.show()
-"""
+        leg.get_frame().set_alpha(0.4)
+        leg.draggable(state=True)
+        plt.show()
 
-# reconstructs original matrix from factorization with k most significant features
-def reconstruct(U, S, Vt, k, t_shape):
-    S_ = np.zeros(t_shape)
-    rows, cols = np.diag_indices(min(S_.shape))
-    S_[rows, cols] = S
+    def reconstruct(self, U, S, Vt, k, t_shape):
+        """ reconstructs original matrix from factorization with k most significant features """
+        S_ = np.zeros(t_shape)
+        rows, cols = np.diag_indices(min(S_.shape))
+        S_[rows, cols] = S
+        return U[:,0:k].dot(S_[0:k,0:k].dot(Vt[0:k,:]))
 
-    return U[:,0:k].dot(S_[0:k,0:k].dot(Vt[0:k,:]))
+    def evaluate(self):
+        """ Evaluates accuracy of predictions """
+        print('RMSE: ', self.RMSE(self.test_data, self.predictions))
 
-# try all k = 7 seems to be the best
-k = 7
-# reconstruct matrix
-predicted = reconstruct(U, S, Vt, k, train_data.shape) + mean
-# evaluate offline performance
-performance(predicted, 'SVD baseline k = {0}'.format(k))  
-
-def parse(line):
-    """ parses line and returns parsed row, column and value """
-    m = re.search('r(.+?)_c(.+?),(.+?)', line.decode('utf-8'))
-    row = int(m.group(1))
-    column = int(m.group(2))
-    value = int(m.group(3))
-    return row, column, value
-
-with open('../data/svd.csv', 'w+') as f:
-    f.write('Id,Prediction\n')
-    with open('../data/sampleSubmission.csv', 'rb') as f2:
-        content = f2.readlines()
-        content = content[1:]
-        for line in content:
-            if line:
-                row, column, value = parse(line)
-                f.write('r{0}_c{1},{2}\n'.format(row, column, predicted[row-1,column-1]))
+svd = SVD(1)
+svd.train()
+svd.plot_scree()
+svd.evaluate()
--- src/svd2.py
+++ src/svd2.py
@@ -13,11 +13,11 @@ data = loadDataForSurprise()
 trainset = data.build_full_trainset()
 
 # create SVD algorithm and train it
-algo = SVD(n_factors=200)
+algo = SVD(n_factors=14, n_epochs=50, lr_all=0.09, reg_all=0.084)
 algo.fit(trainset)
 
 # save the predictions
-makePredictions(algo, '../data/svd2.csv')
+makePredictions(algo, '../data/svd3.csv')
 
 # validate it
 kf = KFold(random_state=0)
@@ -25,3 +25,5 @@ out = cross_validate(algo, data, ['rmse', 'mae'], kf)
 
 # print errors
 print("RMSE: {0}, MAE: {1}".format(np.mean(out['test_rmse']), np.mean(out['test_mae'])))
+
+# factors=1, epochs=50 ==> RMSE=0.9985101082212786
